a8864ec47a12c7a1b568a597595bfb117418d30d
Ashley Madera <ashleymadera@users.github.com>
2020-06-19 23:51:05 -0400

    reate app created, components created, css added

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+      <img src="https://i.imgur.com/lesE9qs.jpg" alt="palm leaves" />
+
+      <section>
+        <div className='white-circle'>
+          <div className='gold-border'>
+            <h2>Ashley Madera</h2>
+            <h3>Software Engineer</h3>
+          </div>
+        </div>
+
+      </section>
+
+    </header>
+  )
+}

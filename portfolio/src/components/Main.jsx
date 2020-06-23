dac500060f1ba49c538baf9c78daad0ab0f8a038
Ashley Madera <ashleymadera@users.github.com>
2020-06-22 22:12:36 -0400

    links added

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -7,23 +7,33 @@ export default function Main() {
 
       <div className='resume-blog-box'>
         <section>
-          <a href="">Resume</a>
+          <a href="https://docdro.id/oToLMit">
+            <img classname="resume" src="https://i.imgur.com/rHAOLJm.png" alt="Resume" />
+          </a>
         </section>
         <section>
-          <a href="https://medium.com/@ashleymadera"> Blog</a>
+          <a href="https://medium.com/@ashleymadera">
+            <img className="blog" src="https://i.imgur.com/spg2A6a.png" alt="Medium Blog logo" />
+          </a>
         </section>
       </div>
 
       <main className='projects'>
         <section className='project-one'>
-          <a href="https://ashleymadera.github.io/POE/"> POE </a>
+          <a href="https://ashleymadera.github.io/POE/">
+            <img className="one-image" src="" alt="" />
+          </a>
         </section>
         <section className='project-two'>
-          project two
-      </section>
+          <a href="">
+            <img classname="two-image" src="" alt="" />
+          </a>
+        </section>
         <section className='project-three'>
-          project three
-      </section>
+          <a href="">
+            <img classname="three-image" src="" alt="" />
+          </a>
+        </section>
       </main>
     </>
   )
